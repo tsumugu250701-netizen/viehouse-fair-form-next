b1582acc407e59998f914a480f9c54a773a18559
tsumugu250701-netizen <tsumugu250701@gmail.com>
2026-05-23 03:09:20 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -146,6 +146,7 @@ export default function Page() {
       </label>
     ))}
   </div>
+</div>
 
               <div className="question">
                 <h2>{showGift ? 'Q6.' : 'Q5.'} ご質問・ご要望があればご入力ください</h2>
